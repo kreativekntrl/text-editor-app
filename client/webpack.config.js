a39e9a7caf6a15512b0c4d991594708d4da824ed
Ava <avamsteimle@gmail.com>
2022-03-01 20:12:58 -0800

    deploy

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -5,10 +5,6 @@ const {
   InjectManifest
 } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
